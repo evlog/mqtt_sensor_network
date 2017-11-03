6f02d6e151fb015e2d1fd40a3387c230e15b3d8a
Vrettel <giovrettos@yahoo.gr>
2017-11-03 09:01:00 +0200

    A python test module
    This module is/will be used as the main body of the program.
    
    At this point, it contains only module imports. In the future it will be used for object creation and project initialization (based on OOP).

---
+++ server/python/monitor.py
@@ -0,0 +1,18 @@
+"""
+    File name: monitor.py
+    Author: Georgios Vrettos
+    Date created: 02/11/2017
+    Date last modified: 03/11/2017
+    Python Version: 2.7
+
+
+Example:
+	$ python monitor.py
+
+Todo:
+	* Create object instances from classes.
+"""
+from mqtt_handler import *
+# --------------------------------------------------------------------------------------------------
+
+
