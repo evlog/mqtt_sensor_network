78f10f431ec40524ddc2b87d0dfd21e6e52a4595
Vrettel <giovrettos@yahoo.gr>
2017-11-06 23:23:38 +0200

    Updated version of the database handler using OOP.

--- server/python/database_handler.py
+++ server/python/database_handler.py
@@ -2,14 +2,12 @@
     File name: database_handler.py
     Author: Georgios Vrettos
     Date created: 02/11/2017
-    Date last modified: 03/11/2017
+    Date last modified: 06/11/2017
     Python Version: 2.7
 
-In this module, functions from the package "psycopg2" are used. To install the package using pip issue the following command:
+In this module, functions from the package "psycopg2" are used. 
+To install the package using pip issue the following command:
 $pip install psycopg2  
-
-Todo :
-	* Add Static variables such as ip, port etc
 
 """
 
@@ -22,36 +20,41 @@ import psycopg2
 # --------------------------------------------------------------------------------------------------
 
 
-def connect_db(record):
-    """connect_db function. This function is used for the database connection.
-    Args:
-        param1 (str): The record to be inserted into the database.
-    """
+class DbHandler:
 
-    #Connecting to database using the connect function from the "psycopg2" package. As parameters we use the database name, username and password as well as host address and port.
-    try:
-        conn = psycopg2.connect(database="IoT_thesis", user="thesis", password="m3Fg9aqZX@8brmg8", host="195.251.166.202", port="5432")
+    def connect_db(self,record):
+        """connect_db function. This function is used for the database connection.
+        Args:
+            param1 (DbHandler): Object instance
+            param2 (str): The record to be inserted into the database.
+        """
 
-        #After a successfull connection, we call the insert funtion.
-        insert_db(conn,record)
-    except psycopg2.DatabaseError, exception:
-        print exception
-    finally:
-        conn.close()
+        #Connecting to database using the connect function from the "psycopg2" package.
+        # As parameters we use the database name, username and password as well as host address and port.
+        try:
+            conn = psycopg2.connect(database="IoT_thesis", user="thesis", password="m3Fg9aqZX@8brmg8", host="localhost", port="5432")
+
+            #After a successfull connection, we call the insert funtion.
+            self.insert_db(conn,record)
+        except psycopg2.DatabaseError, exception:
+            print exception
+        finally:
+            conn.close()
 
 
-def insert_db(conn,data):
-    """insert_db function. This function is used to insert the string into the json file inside the database.
-    Args:
-        param1 (conn): The connection instance
-        param2 (str): The data to be inserted into the dataabase
-    """
+    def insert_db(self,conn,data):
+        """insert_db function. This function is used 
+        to insert the string into the json file inside the database.
+        Args:
+            param1 (conn): The connection instance
+            param2 (str): The data to be inserted into the dataabase
+        """
 
-    #A cursor for handling the queries is created
-    cur = conn.cursor()
-    #The final insertion query
-    query = "INSERT INTO readings (data) VALUES ('" + data + "');"
-    #Query execution and database commit.
-    cur.execute(query)
-    conn.commit()
-    print "Record inserted successfully"
+        #A cursor for handling the queries is created
+        cur = conn.cursor()
+        #The final insertion query
+        query = "INSERT INTO readings (data) VALUES ('" + data + "');"
+        #Query execution and database commit.
+        cur.execute(query)
+        conn.commit()
+        print "Record inserted successfully"
