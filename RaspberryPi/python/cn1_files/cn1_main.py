0a796db531080f98e3961eec29b0f753680a1d42
Vrettel <giovrettos@yahoo.gr>
2017-12-17 14:32:52 +0200

    CN1 main module
    Initiates state machine object

---
+++ RaspberryPi/python/cn1_files/cn1_main.py
@@ -0,0 +1,28 @@
+"""
+    File name: cn1_main.py
+    Author: Georgios Vrettos
+    Date created: 10/12/2017
+    Date last modified: 14/12/2017
+    Python Version: 2.7
+
+
+Example:
+	$ python cn1_main.py
+
+Todo :
+	* 
+
+"""
+
+from cn1_machine import Client1Machine
+
+
+# Global variables
+# --------------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------------
+
+# The instance of the state machine.
+machine = Client1Machine()
+#machine.on_event("active")
